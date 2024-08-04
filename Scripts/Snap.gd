62f7ea0c9bcd5aea5d2f5279276e0b33e22023f4
Rémi Leroy <remi.leroyy@gmail.com>
2024-08-04 18:40:49 +0200

    still trying to make snapping rotations

--- Scripts/Snap.gd
+++ Scripts/Snap.gd
@@ -1,13 +1,14 @@
 extends Area3D
 
-func _physics_process(_delta):
-	if $Joint.node_a:
-		return
-	for snap in get_overlapping_areas():
-		if snap.get_parent().name > get_parent().name:
-			#get_parent().global_rotation = snap.global_rotation - get_parent().basis * global_rotation
-			get_parent().global_position = snap.global_position - get_parent().basis * position
-			$Joint.node_a = get_parent().get_path()
-			$Joint.node_b = snap.get_parent().get_path()
+#func _physics_process(_delta):
+	#if $Joint.node_a:
+		#return
+	#for snap in get_overlapping_areas():
+		#if snap.get_parent().name < get_parent().name:
+			#print(get_parent().global_rotation_degrees, rotation_degrees, global_rotation_degrees)
+			#print(snap.get_parent().global_rotation_degrees, snap.rotation_degrees)
+			##get_parent().global_position = snap.global_position - get_parent().basis * position
+			#$Joint.node_a = get_parent().get_path()
+			#$Joint.node_b = snap.get_parent().get_path()
 			
 			
